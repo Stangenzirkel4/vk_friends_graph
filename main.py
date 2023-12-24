4ff85e98ecfd56357f99e596b674d817a050c1b1
Stangenzirkel4 <113057495+Stangenzirkel4@users.noreply.github.com>
2023-12-24 19:10:31 +0300

    Added functions and laplacian

--- main.py
+++ main.py
@@ -4,26 +4,7 @@ import os
 import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
-    # Удаляет данные от предыдущего запуска
-    os.remove("grid.edgelist")
-
-    # Нужно, чтобы не удалять пароль при каждом коммите
-    with open('pas.txt') as f:
-        temp = f.read().splitlines()
-        login = temp[0]       # Логин и пароль для входа
-        password = temp[1]
-        user_id = temp[2]         # id человека, чьих друзей мы ищем
-
-    # Вы можете записать логин и пароль в файл 'pas.txt' или просто присвоить переменным значения
-    # login = '123123'
-    # password = 'qwerty'
-    # user_id = '000000001'
-
-    # авторизация Вк
-    vk = vk_api.VkApi(login=login, password=password, app_id=2685278)
-    vk.auth()
-
+def make_gridlist(user_id):     
     if not user_id:
         person = vk.method('users.get')     # Возвращает данные о пользователе, чей логин был введен
         user_id = person[0]['id']
@@ -85,6 +66,7 @@ if __name__ == "__main__":
             print('Cant get mutuals of id' + str(person_id), 'with id' + str(friend['id']))
     f.close()
 
+def draw_graph():
     # Создаем граф, считывая данные о вершинах и ребрах из файла
     G = nx.read_edgelist(path="grid.edgelist", delimiter=":")
 
@@ -97,18 +79,59 @@ if __name__ == "__main__":
         'with_labels': True  # печатать ли заголовки узлов
     }
     # Рисуем граф с помощью функции draw
-    nx.draw(G, pos=nx.draw_circular(), **options)
-    # параметр pos отвечает за то, как будет отображен граф
-    # вы можете попробовать использовать следующие варианты
-    # nx.draw(G, pos=nx.draw_circular(G), **options) -
-    # nx.draw(G, pos=nx.draw_circular(G), **options) -
-    # nx.draw(G, pos=nx.draw_circular(G), **options) -
-
-    # С помощью метода библиотеки networkx получаем лапласиан графа
-    L = nx.laplacian_matrix(G)
-    L_array = L.toarray()
+    nx.draw_spectral(G, **options)
+    # вы также можете попробовать использовать следующие варианты
+    # nx.draw_circular(G, **options) -
+    # nx.draw_shell(G, **options) -
+    # nx.draw_spiral(G, **options) -
+    # nx.draw_spectral(G, **options) -
 
     # устанавливаем размер изображения в дюймах
     plt.gcf().set_size_inches(40, 40)
     # Сохраняем граф в файл
     plt.savefig('graph.png')
+
+def get_laplacian():
+
+    G = nx.read_edgelist(path="grid.edgelist", delimiter=":")
+
+    # С помощью метода библиотеки networkx получаем лапласиан графа
+    L = nx.laplacian_matrix(G)
+    L_array = L.toarray()
+    with open('Laplacian.txt', 'w') as f:
+        for l in L_array:
+            line = ""
+            for i in l:
+                line += (str(i)) + "    "
+            line += "\n"
+            f.write(line)
+
+
+if __name__ == "__main__":
+    # Удаляет данные от предыдущего запуска
+    try:
+        os.remove("grid.edgelist")
+    except:
+        print("Nothing to delete")
+
+    # Нужно, чтобы не удалять пароль при каждом коммите
+    with open('pas.txt') as f:
+        temp = f.read().splitlines()
+        login = temp[0]       # Логин и пароль для входа
+        password = temp[1]
+        user_id = temp[2]         # id человека, чьих друзей мы ищем
+
+    # Вы можете записать логин и пароль в файл 'pas.txt' или просто присвоить переменным значения
+    # login = '123123'
+    # password = 'qwerty'
+    # user_id = '000000001'
+
+    # авторизация Вк
+    vk = vk_api.VkApi(login=login, password=password, app_id=2685278)
+    vk.auth()
+
+    make_gridlist(user_id)
+    draw_graph()
+    get_laplacian()
+
+
